cc6d67d1ae076fcef926ff4e9f9043e1c345485d
Léo Generet <lpgeneret@gmail.com>
2022-03-31 17:04:18 +0200

    :sparkles: First iteration on the client

---
+++ dist/index.672d4772.js
@@ -0,0 +1,4 @@
+console.log('HELLO');
+var gameList = document.querySelector('.gameList');
+
+//# sourceMappingURL=index.672d4772.js.map
---
+++ games.js
@@ -0,0 +1,9 @@
+const games = [
+    {
+        "id": 0,
+        "game": 'Super Mario',
+        "link": "https://facbook.com"
+    }
+]
+
+export default games
