fa5b6a35d60b173a1800a235968744aa9e6e0d6c
Léo Generet <lpgeneret@gmail.com>
2022-05-04 12:01:21 +0200

    Add node_modules

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('HELLO')
+console.log('HELLO');
+
+window.addEventListener('keydown', event => {
+    console.log(event);
+    if (event.isComposing || event.code === 'ArrowRight') {
+        console.log('Right️');
+    }
+
+    if (event.isComposing || event.code === 'ArrowLeft') {
+        console.log('Left');
+    }
+});
